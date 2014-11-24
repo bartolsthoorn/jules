109588e5d6dea395f234a2412d85b0015c93a7a0
Bart Olsthoorn <bartolsthoorn@gmail.com>
2014-11-24 16:23:52 +0000

    Restart jules project

--- lib/damerau_levenshtein/sugar.rb
+++
@@ -1,9 +0,0 @@
-module DamerauLevenshtein
-  # returns 1.0 for completely different strings
-  # returns 0.0 for completely identical strings
-  def self.relative(a, b)
-    length = [a.length, b.length].max
-    return DamerauLevenshtein.distance(a, b).to_f / length
-  end
-end
-DL = DamerauLevenshtein
--- spec/lib/jules_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Jules do
-  it { should respond_to :❨╯°□°❩╯︵┻━┻}
-end
